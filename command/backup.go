8c24fe3d3f22ab6638fe6f5d05b60bd80931f059
Jake Champlin <jake.champlin.27@gmail.com>
2016-09-30 17:11:46 -0400

    Add -once flag to backup command
    Adds `-once` flag to backup command, to run a single backup once.
    
    Fixes: #6

--- command/backup.go
+++ command/backup.go
@@ -1,8 +1,10 @@
 package command
 
 import (
+	"flag"
 	"fmt"
 
+	"github.com/mitchellh/cli"
 	"github.com/pshima/consul-snapshot/backup"
 )
 
@@ -14,8 +16,17 @@ type BackupCommand struct {
 
 // Run the backup via backup.Runner
 func (c *BackupCommand) Run(args []string) int {
+	// Set flags
+	var flagOnce bool
+	fs := flag.NewFlagSet("backup", flag.ContinueOnError)
+	fs.BoolVar(&flagOnce, "once", false, "")
+	// Parse flags
+	if err := fs.Parse(args); err != nil {
+		return cli.RunResultHelp
+	}
+
 	c.UI.Info(fmt.Sprintf("v%v: Starting Consul Snapshot", c.Version))
-	response := backup.Runner(c.Version)
+	response := backup.Runner(c.Version, flagOnce)
 	// Actually need to return the proper response here.
 	return response
 }
@@ -31,5 +42,8 @@ func (c *BackupCommand) Help() string {
 Usage: consul-snapshot backup
 
 Starts a backup that repeats over an interval.
+
+Options:
+  -once           Run consul-snapshot only once, instead of running on an interval
 `
 }
